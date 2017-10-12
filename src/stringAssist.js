a59f5df0677a27769f510e78b16c87ef14f40fe5
Joy-Lai <pefish@qq.com>
2017-10-12 21:18:23 +0800

    add addThousandSign method of String

--- src/stringAssist.js
+++ src/stringAssist.js
@@ -4,23 +4,25 @@
 import BigNumber from 'bignumber.js'
 import AssertUtil from './common/utils/AssertUtil'
 
-function assertType (val) {
-  if (typeof val !== 'string') {
-    throw new Error(`'string' type can not compare to '${typeof val}' type`)
-  }
-}
-
 String.prototype.equals = function (val) {
   AssertUtil.isType(val, 'string')
   return this === val
 }
 
-String.prototype.add = function (val) {
+String.prototype.add = function (val, retBigNumber = false) {
   AssertUtil.isType(val, ['string', BigNumber])
-  const num1 = new BigNumber(this)
-  const num2 = (val instanceof BigNumber) ? val : new BigNumber(val)
-  return num1.add(num2).toString()
+  const BN = BigNumber.another({
+    EXPONENTIAL_AT: 1e+9
+  })
+  const num1 = new BN(this)
+  return retBigNumber === true ? num1.add(val) : num1.add(val).toString()
 }
+
+// 加千分号
+String.prototype.addThousandSign = function (decimalRemain = null, remainMethod = BigNumber.ROUND_HALF_UP) {
+  return new BigNumber(this).toFormat(decimalRemain, remainMethod)
+}
+
 
 
 
--- test/index.js
+++ test/index.js
@@ -1,22 +1,24 @@
 /**
  * Created by joy on 12/10/2017.
  */
-require('../index')
+require('../index_es6')
 
 // String
 // console.log('1'.equals(1))  // exception
 // console.log('1'.equals('1'))  // true
 // console.log('1'.equals('2'))  // false
 
-const b = '1111111111111111111', c = 1
+// const b = '11111111111111111111111', c = 1
 // console.log(Number(b) + c) // 1111111111111111200 有精度问题
-console.log(b.add(c))  // exception
+// console.log(b.add(c))  // exception
 // console.log(b.add(c.toString()))  // 1111111111111111112
 // console.log(b.add(new BigNumber(c.toString())))  // 1111111111111111112
 
-// const a = '0.1', b = 0.2
+const a = '36563.12542', b = 0.2
 // console.log(a + b)  // 0.30000000000000004 有精度问题
 // console.log(a.add(b.toString()))  // 0.3
+// console.log(a.addThousandSign())
+
 
 // Number
 // const a = 1
