dcf1c91c671a6dadac780142b52e7e0161ce4e12
Joy-Lai <pefish@qq.com>
2017-10-17 16:17:34 +0800

    0.0.4

--- src/stringAssist.js
+++ src/stringAssist.js
@@ -18,6 +18,15 @@ String.prototype.add = function (val, retBigNumber = false) {
   return retBigNumber === true ? num1.add(val) : num1.add(val).toString()
 }
 
+String.prototype.multi = function (val, retBigNumber = false) {
+  AssertUtil.isType(val, ['string', BigNumber])
+  const BN = BigNumber.another({
+    EXPONENTIAL_AT: 1e+9
+  })
+  const num1 = new BN(this)
+  return retBigNumber === true ? num1.multi(val) : num1.multi(val).toString()
+}
+
 // 加千分号
 String.prototype.addThousandSign = function (decimalRemain = null, remainMethod = BigNumber.ROUND_HALF_UP) {
   return new BigNumber(this).toFormat(decimalRemain, remainMethod)
