7257b0fbff19556c900619944dd89411229c7a3a
Joy-Lai <pefish@qq.com>
2017-10-17 18:27:19 +0800

    1

--- src/stringAssist.js
+++ src/stringAssist.js
@@ -18,13 +18,49 @@ String.prototype.add = function (val, retBigNumber = false) {
   return retBigNumber === true ? num1.add(val) : num1.add(val).toString()
 }
 
+String.prototype.sub = function (val, retBigNumber = false) {
+  AssertUtil.isType(val, ['string', BigNumber])
+  const BN = BigNumber.another({
+    EXPONENTIAL_AT: 1e+9
+  })
+  const num1 = new BN(this)
+  return retBigNumber === true ? num1.sub(val) : num1.sub(val).toString()
+}
+
 String.prototype.multi = function (val, retBigNumber = false) {
   AssertUtil.isType(val, ['string', BigNumber])
   const BN = BigNumber.another({
     EXPONENTIAL_AT: 1e+9
   })
   const num1 = new BN(this)
-  return retBigNumber === true ? num1.multi(val) : num1.multi(val).toString()
+  return retBigNumber === true ? num1.times(val) : num1.times(val).toString()
+}
+
+String.prototype.div = function (val, retBigNumber = false) {
+  AssertUtil.isType(val, ['string', BigNumber])
+  const BN = BigNumber.another({
+    EXPONENTIAL_AT: 1e+9
+  })
+  const num1 = new BN(this)
+  return retBigNumber === true ? num1.div(val) : num1.div(val).toString()
+}
+
+String.prototype.gt = function (val) {
+  AssertUtil.isType(val, ['string', BigNumber])
+  const num1 = new BigNumber(this)
+  return num1.comparedTo(val) === 1
+}
+
+String.prototype.lt = function (val) {
+  AssertUtil.isType(val, ['string', BigNumber])
+  const num1 = new BigNumber(this)
+  return num1.comparedTo(val) === -1
+}
+
+String.prototype.eq = function (val) {
+  AssertUtil.isType(val, ['string', BigNumber])
+  const num1 = new BigNumber(this)
+  return num1.comparedTo(val) === 0
 }
 
 // 加千分号
