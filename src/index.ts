2d61e79796e6fb2d98cd58603f02dadbc7436ecb
joy <joy.lai@zg.com>
2019-04-28 16:17:10 +0800

    add

--- src/index.ts
+++ src/index.ts
@@ -7,3 +7,12 @@ require('./buffer')
 require('./number')
 require('./promise')
 require('./bignumber')
+
+export * from './error'
+export * from './string'
+export * from './array'
+export * from './buffer'
+export * from './number'
+export * from './promise'
+export * from './bignumber'
+
---
+++ types/index.d.ts
@@ -0,0 +1,7 @@
+export * from './error';
+export * from './string';
+export * from './array';
+export * from './buffer';
+export * from './number';
+export * from './promise';
+export * from './bignumber';
