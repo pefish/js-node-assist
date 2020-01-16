887caa03905b0bc9a63b19b43e7013aa992aaa4d
joy <joy.lai@zg.com>
2020-01-16 20:02:57 +0800

    add

--- types/string.d.ts
+++ types/string.d.ts
@@ -60,4 +60,16 @@ declare global {
         removeFirstStr_: (str: string) => string;
     }
 }
+export declare enum RoundingMode {
+    ROUND_UP = 0,
+    ROUND_DOWN = 1,
+    ROUND_CEIL = 2,
+    ROUND_FLOOR = 3,
+    ROUND_HALF_UP = 4,
+    ROUND_HALF_DOWN = 5,
+    ROUND_HALF_EVEN = 6,
+    ROUND_HALF_CEIL = 7,
+    ROUND_HALF_FLOOR = 8,
+    EUCLID = 9
+}
 export {};
